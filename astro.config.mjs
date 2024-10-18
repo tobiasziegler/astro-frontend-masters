3c2f2fc5b2194b8b77c56e5a9f9cd15b2c5ce124
David Mallard <correspondence@davidmallard.id.au>
2024-10-18 17:24:02 +1100

    Complete Adding an RSS Feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-frontend-masters-tobiasziegler.netlify.app',
 	integrations: [
 		react({
 			include: ['**/react/*'],
