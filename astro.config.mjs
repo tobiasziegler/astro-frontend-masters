38206981588bfe252bca6623d3eae2e9e55c529a
David Mallard <correspondence@davidmallard.id.au>
2024-10-18 17:42:53 +1100

    Complete Enabling SSR & Forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-frontend-masters-tobiasziegler.netlify.app',
+	output: 'hybrid',
+	experimental: {
+		hybridOutput: true,
+	},
 	integrations: [
 		react({
 			include: ['**/react/*'],
