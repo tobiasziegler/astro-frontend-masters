6f72a36919efc425cbb2845261336d358d71bd29
David Mallard <correspondence@davidmallard.id.au>
2024-10-19 17:05:58 +1100

    Prepare to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-frontend-masters-tobiasziegler.netlify.app',
-	output: 'hybrid',
-	experimental: {
-		hybridOutput: true,
-	},
-	integrations: [
-		react({
-			include: ['**/react/*'],
-		}),
-		solidJs({
-			include: ['**/solid/*'],
-		}),
-	],
+  site: 'https://astro-frontend-masters-tobiasziegler.netlify.app',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [react({
+    include: ['**/react/*']
+  }), solidJs({
+    include: ['**/solid/*']
+  })],
+  adapter: netlify()
 });
